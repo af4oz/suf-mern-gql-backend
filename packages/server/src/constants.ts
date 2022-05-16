7603828050343d6c54186b55c4f1f723d44fe3ea
zkindest <zkindest5@gmail.com>
2022-05-16 13:14:54 +0530

    Add regex to cors origin

--- packages/server/src/constants.ts
+++ packages/server/src/constants.ts
@@ -3,7 +3,6 @@ export const IS_PROD = process.env.NODE_ENV === 'production'
 export const FRONTEND_URLS = IS_PROD
   ? [
       'https://suf-mern-gql-frontend.vercel.app',
-      'https://suf-mern-gql-frontend-zkindest.vercel.app',
-      'https://suf-mern-gql-frontend-git-main-zkindest.vercel.app',
+      /https:\/\/suf-mern-gql-frontend(.*-zkindest)?.vercel.app/,
     ]
   : ['http://localhost:3000']
