8b307d6c27a0f48a3e8cae6a6b272d4da82bcc7a
af4oz <0xafz.dev@gmail.com>
2025-01-24 19:09:21 +0530

    Update

--- packages/server/src/utils/config.ts
+++ packages/server/src/utils/config.ts
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT
-export const MONGODB_URI = process.env.MONGODB_URI
-export const JWT_SECRET = process.env.JWT_SECRET
+export const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://root:example@localhost:27017/' // TODO: Don't run this in Production
+export const JWT_SECRET = process.env.JWT_SECRET || 'RandomJWTSecret' // TODO: Don't run this in Production
