07a6fecc893ac456c4d205163e25c6edbdd3e7fb
af4oz <0xafz.dev@gmail.com>
2025-01-24 20:28:54 +0530

    Update

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -3,13 +3,11 @@ import { MONGODB_URI as url } from './utils/config'
 
 const connectToDB = async () => {
   if (!url) throw new Error('Provide valid Mongodb Url!')
-  try {
-    await mongoose.connect(url)
+  await mongoose.connect(url, {
+    serverSelectionTimeoutMS: 5000,
+  })
 
-    console.log('Connected to MongoDB!')
-  } catch (error) {
-    console.error(`Error while connecting to MongoDB: `, error)
-  }
+  console.log('Connected to MongoDB!')
 }
 
 export default connectToDB
